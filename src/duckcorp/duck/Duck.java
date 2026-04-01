9e99098933e41d66ae7eede5866a556ef9e67c36
Benny <benji.locin@gmail.com>
2026-04-01 09:26:05 +0200

    feat: Implement equals and hashCode in Duck.java

--- src/duckcorp/duck/Duck.java
+++ src/duckcorp/duck/Duck.java
@@ -2,6 +2,8 @@ package duckcorp.duck;
 
 import duckcorp.duck.Qualifiable;
 
+import java.util.Objects;
+
 /**
  * Classe abstraite représentant un canard en plastique.
  *
@@ -46,21 +48,20 @@ public abstract class Duck implements Qualifiable {
 
     public abstract String describe();
 
-    // --- TODO : equals et hashCode ---
-
     /**
      * Deux canards sont égaux si et seulement si ils ont le même identifiant.
-     * TODO : implémentez equals() en vous basant uniquement sur le champ id.
      */
     @Override
     public boolean equals(Object o) {
-        throw new UnsupportedOperationException("TODO : Duck.equals()");
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Duck duck = (Duck) o;
+        return this.hashCode() == duck.hashCode();
     }
 
-    /** TODO : implémentez hashCode() de façon cohérente avec equals(). */
     @Override
     public int hashCode() {
-        throw new UnsupportedOperationException("TODO : Duck.hashCode()");
+        return  Objects.hash(id);
     }
 
     // --- toString fourni ---
